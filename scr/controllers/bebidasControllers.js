2fd6b1234c903fa3854152e50e4a81fd85540a34
layconjohn <layconjohn@gmail.com>
2022-07-10 21:18:22 -0300

    feat: Opção de deletar e atualizar bebidas

--- scr/controllers/bebidasControllers.js
+++ scr/controllers/bebidasControllers.js
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb';
 import db from '../databases/mongodb.js';
 import { pedidoSchema } from '../schemas/pedidosSchema.js';
 
@@ -33,4 +34,35 @@ export async function adicionarBebidas(req, res) {
         console.error("Erro ao cadastrar uma bebida")
         res.sendStatus(500);
     }
+}
+
+export async function atualizarBebida(req, res) {
+    const { id } = req.params;
+
+    try {
+        const bebida = await db.collection("bebidas").findOne( {_id: new ObjectId(id)});
+
+        if (!bebida) {
+            res.status(404).send("Bebida não encontrada")
+        }
+
+        await db.collection("bebidas").updateOne( {_id: bebida._id}, {$set: req.body})
+        res.status(200).send("Bebida atualizada com sucesso!");
+    } catch (error) {
+        console.error("Erro ao atualizar as bebidas", error);
+        res.sendStatus(500);
+    }
+}
+
+export async function deletarBebida(req, res) {
+    const { id } = req.params;
+
+    try {
+        await db.collection("bebidas").deleteOne( {_id: new ObjectId(id)})
+        res.status(200).send("Bebida deletada com sucesso!");
+    } catch (error) {
+        console.error("Erro ao atualizar as bebidas", error);
+        res.sendStatus(500);
+    }
+
 }
--- scr/routes/bebidasRoutes.js
+++ scr/routes/bebidasRoutes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
-import { pegarBebidas, adicionarBebidas } from '../controllers/bebidasControllers.js';
+import { pegarBebidas, adicionarBebidas, atualizarBebida, deletarBebida } from '../controllers/bebidasControllers.js';
 
 const router = Router();
 
 router.get('/bebida', pegarBebidas);
 router.post('/bebida', adicionarBebidas);
+router.put('/bebida/:id', atualizarBebida);
+router.delete('/bebida/:id', deletarBebida);
 
 export default router;
